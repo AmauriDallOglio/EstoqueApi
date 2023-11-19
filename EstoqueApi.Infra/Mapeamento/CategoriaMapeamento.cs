f74737183d027868d873745a7a84a26d53d8118c
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2023-11-19 15:15:13 -0300

    Criação da classe mapeamento

---
+++ EstoqueApi.Infra/Mapeamento/CategoriaMapeamento.cs
@@ -0,0 +1,17 @@
+﻿using EstoqueApi.Dominio.Entidade;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EstoqueApi.Infra.Mapeamento
+{
+	public class CategoriaMapeamento : IEntityTypeConfiguration<Categoria>
+	{
+		public void Configure(EntityTypeBuilder<Categoria> builder)
+		{
+			builder.ToTable("CATEGORIA");
+			builder.Property(p => p.Id).HasColumnName("ID").UseIdentityColumn();
+
+			builder.Property(p => p.Descricao).HasColumnName("DESCRICAO").HasColumnType("varchar").HasMaxLength(50).IsRequired();
+		}
+	}
+}
---
+++ EstoqueApi.Infra/Mapeamento/ProdutoMapeamento.cs
@@ -0,0 +1,17 @@
+﻿using EstoqueApi.Dominio.Entidade;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EstoqueApi.Infra.Mapeamento
+{
+	public class ProdutoMapeamento : IEntityTypeConfiguration<Produto>
+	{
+		public void Configure(EntityTypeBuilder<Produto> builder)
+		{
+			builder.ToTable("PRODUTO").HasOne(n => n.Estoque).WithOne(n => n.Produto).OnDelete(DeleteBehavior.Cascade).HasForeignKey<Estoque>(f => f.ProdutoId);
+			builder.HasMany(n => n.Categorias).WithMany(n => n.Produtos);
+			builder.Property(p => p.Id).HasColumnName("ID").UseIdentityColumn();
+			builder.Property(p => p.Descricao).HasColumnName("DESCRICAO").HasColumnType("varchar").HasMaxLength(300).IsRequired();
+		}
+	}
+}
