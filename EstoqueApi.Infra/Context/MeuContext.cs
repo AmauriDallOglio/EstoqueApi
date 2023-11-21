c4cd02559419850a50d224684c6638eabee1b1cc
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2023-11-21 20:43:26 -0300

    Criação do repositorio generico e configuração da api

---
+++ EstoqueApi.Infra/Context/MeuContext.cs
@@ -0,0 +1,21 @@
+﻿using EstoqueApi.Dominio.Entidade;
+using Microsoft.EntityFrameworkCore;
+
+namespace EstoqueApi.Infra.Context
+{
+	public class MeuContext : DbContext
+	{
+		public MeuContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Produto> Produtos { get; set; }
+		public DbSet<Categoria> Categorias { get; set; }
+		public DbSet<Estoque> Estoques { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuContext).Assembly);
+		}
+	}
+}
--- EstoqueApi/Program.cs
+++ EstoqueApi/Program.cs
@@ -1,3 +1,9 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +13,64 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers().AddNewtonsoftJson(opt =>
+				opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+builder.Services.AddControllers();
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+builder.Services.AddDbContextPool<DbContext, PrateleiraDbContext>(options =>
+{
+	options.UseSqlServer(configuration.GetConnectionString("sql_connection"));
+});
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Version = "v1",
+		Title = "Minha.API",
+		Description = "API CRUD",
+		TermsOfService = new Uri("https://example.com/terms"),
+		Contact = new OpenApiContact
+		{
+			Name = "Amauri",
+			Email = "amauri@hotmail.com",
+			Url = new Uri("https://github.com/amauridalloglio"),
+		},
+		License = new OpenApiLicense
+		{
+			Name = "User",
+			Url = new Uri("https://example.com/license"),
+		}
+	});
+});
+
+
+
 var app = builder.Build();
+
+
+app.UseDeveloperExceptionPage();
+
+
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API");
+	c.RoutePrefix = string.Empty;
+});
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapControllers();
+	endpoints.MapGet("/", async context =>
+	{
+		await context.Response.WriteAsync("Hello World!");
+	});
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
